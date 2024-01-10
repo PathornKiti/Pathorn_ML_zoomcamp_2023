37b88d0e25af6918de997da0123e38dad43f1c6d
PathornKiti <pathorn.kiti@gmail.com>
2024-01-10 15:07:10 +0700

    prepare for cloud deploy

--- capstone_project2/backend/main.py
+++ capstone_project2/backend/main.py
@@ -29,11 +29,4 @@ async def predict(request: Img):
         return prediction
 
 app.include_router(router)
-if __name__ == '__main__':
-    uvicorn.run("main:app",
-                host="0.0.0.0", 
-                port=8000, 
-                reload=True,
-                log_level="debug",
-                proxy_headers=True)
 
